b569b7f730fde6448caf1991ad15565ca0300fb0
Srinivas831 <hsrinivas802@gmail.com>
2023-10-30 16:28:36 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,9 @@ app.use(express.json());
 
 app.use("/users",userRouter);
 app.use("/posts",postRouter);
-
+app.get("/",()=>{
+    console.log("backend belpoyed");
+})
 app.listen(8080,async()=>{
     try{
         await connection;
